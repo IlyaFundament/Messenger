e8f394a0329368543727aee460934ea557b735ad
Ilya Fundament <fundamentilya@gmail.com>
2025-08-10 11:40:03 +0300

    [Messenger1]Refactor: Rename MainActivity to LoginActivity and update layout
    This commit renames `MainActivity` to `LoginActivity` and updates its layout to a login screen.
    
    The following changes were made:
    - Renamed `MainActivity.java` to `LoginActivity.java`.
    - Renamed `activity_main.xml` to `activity_login.xml`.
    - Updated `AndroidManifest.xml` to reflect the activity name change.
    - Modified `activity_login.xml` to include UI elements for email, password, login button, forgot password link, and register link.
    - Added corresponding string resources in `strings.xml`.
    - Updated `LoginActivity.java` to initialize these views and set up basic click listeners for the login button, register text, and forgot password text.
    - Changed the base theme in `themes.xml` and `themes.xml (night)` from `Theme.Material3.DayNight.NoActionBar` to `Theme.Material3.DayNight`.

---
+++ app/src/main/java/com/example/messenger/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.example.messenger;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewTitle;
+    private TextView textViewForgotPassword;
+    private TextView textViewRegister;
+    private Button buttonLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initViews();
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                //login
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // launch intent to register screen
+            }
+        });
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // launch intent to forgot password screen
+            }
+        });
+    }
+
+    private void initViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        editTextEmail = findViewById(R.id.editTextEmail);
+    }
+}
